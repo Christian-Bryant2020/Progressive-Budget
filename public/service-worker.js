a913c4b1beec48ebc9bff18d7f64abeab605f501
Christian Bryant <74256156+Christian-Bryant2020@users.noreply.github.com>
2021-06-12 21:01:37 -0500

    Added to cachie

--- public/service-worker.js
+++ public/service-worker.js
@@ -6,11 +6,15 @@ const FILES_TO_CACHE = [
   '/styles.css',
   '/icons/icon-192x192.png',
   '/icons/icon-512x512.png',
+  "/manifest.webmanifest",
+  'https://cdn.jsdelivr.net/npm/chart.js@2.8.0',
+  'https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css'
 ];
 
-const CACHE = 'static-cache-v2';
+const CACHE_NAME = "static-cache-v2";
 const DATA_CACHE_NAME = "data-cache-v1";
 
+// install
 self.addEventListener("install", function(evt) {
   evt.waitUntil(
     caches.open(CACHE_NAME).then(cache => {
@@ -39,31 +43,33 @@ self.addEventListener("activate", function(evt) {
   self.clients.claim();
 });
 
+// fetch
 self.addEventListener("fetch", function(evt) {
+  // cache successful requests to the API
   if (evt.request.url.includes("/api/")) {
-    console.log('line 45')
     evt.respondWith(
       caches.open(DATA_CACHE_NAME).then(cache => {
         return fetch(evt.request)
           .then(response => {
+            // If the response was good, clone it and store it in the cache.
             if (response.status === 200) {
               cache.put(evt.request.url, response.clone());
             }
 
             return response;
           })
-
           .catch(err => {
+            // Network request failed, try to get it from the cache.
             return cache.match(evt.request);
           });
-
       }).catch(err => console.log(err))
-
     );
 
     return;
   }
 
+  // if the request is not for the API, serve static assets using "offline-first" approach.
+  // see https://developers.google.com/web/fundamentals/instant-and-offline/offline-cookbook#cache-falling-back-to-network
   evt.respondWith(
     caches.match(evt.request).then(function(response) {
       return response || fetch(evt.request);
